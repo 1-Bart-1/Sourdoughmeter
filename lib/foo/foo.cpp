ec83d1b7a5637704966d03b9704f3a7f4b739f79
19nico09 <nicolas.astrup@yahoo.no>
2023-03-03 12:33:55 +0100

    test

---
+++ lib/foo/foo.cpp
@@ -0,0 +1,15 @@
+#include "Arduino.h"
+#include "foo.h"
+
+Math::math(int num)
+{
+    _num = num; 
+}
+Math::multiply(int num)
+{
+    return _num * num;
+}
+Math::devide(int num)
+{
+    return 
+}
